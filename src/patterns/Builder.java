b265df1ae4705e2f23d1cd137cf1e9cfded0e5c5
BhaaRizik <bhaarizik95@gmail.com>
2023-01-05 12:19:30 +0200

    add files

--- src/patterns/Builder.java
+++ src/patterns/Builder.java
@@ -1,5 +1,38 @@
 package patterns;
 
-public class Builder {
+import enums.DesignPatternsTypes;
+
+public class Builder implements DesignPattern {
+
+	private String first;
+	private String second;
+	private String third;
+	private String forth;
+	
+	public Builder addFirst(String s) {
+		first = s;
+		return this;
+	}
+	
+	public Builder addsecond(String s) {
+		second = s;
+		return this;
+	}
+	
+	public Builder addThird(String s) {
+		third = s;
+		return this;
+	}
+	
+	public Builder addForth(String s) {
+		forth = s;
+		return this;
+	}
+	
+	
+	@Override
+	public DesignPatternsTypes getType() {
+		return DesignPatternsTypes.BUILDER;
+	}
 
 }
--- src/patterns/Factory.java
+++ src/patterns/Factory.java
@@ -1,5 +1,45 @@
 package patterns;
 
-public interface Factory {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import enums.DesignPatternsTypes;
+
+public class Factory implements DesignPattern {
+	private final Map<String, Contract> classTypeToClassInstance;
+	
+	public Factory(List<Contract> classes) {
+		this.classTypeToClassInstance = classes.stream().collect(Collectors.toMap(key -> key.getType(), value -> value));
+	}
+	
+	public Contract getInstance(String className) {
+		return classTypeToClassInstance.get(className);
+	}
+	
+	@Override
+	public DesignPatternsTypes getType() {
+		return DesignPatternsTypes.FACTORY;
+	}
+	
+}
+
+interface Contract {
+	String getType();
+}
+
+class A implements Contract {
+	
+	@Override
+	public String getType() {
+		return "A";
+	}
+}
+
+class B implements Contract {
+	
+	@Override
+	public String getType() {
+		return "B";
+	}
 }
